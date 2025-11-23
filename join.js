08556c1b0322fef50eb92004c04fff12181bb348
shalevlevi2010-creator <shalevlevi2010@gmail.com>
2025-11-23 10:42:43 +0200

    Add files via upload

---
+++ join.js
@@ -0,0 +1,16 @@
+let tokens = {};
+
+function generateToken() {
+    return Math.random().toString(36).substr(2, 8);
+}
+
+export default function handler(req, res) {
+    const token = generateToken();
+    const expire = Date.now() + 2*60*1000; // 2 דקות
+    tokens[token] = expire;
+
+    res.writeHead(302, {
+        Location: `https://chat.whatsapp.com/I5gl9o1oPTUKTowihWFZOW?token=${token}`
+    });
+    res.end();
+}
